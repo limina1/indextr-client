3e3c1ed0d086001761084397000f1566d9e3d455
buttercat1791 <mjjurkoic@gmail.com>
2024-09-24 21:23:29 -0500

    Add margins on editor page main content

--- src/routes/new/edit/+page.svelte
+++ src/routes/new/edit/+page.svelte
@@ -23,7 +23,7 @@
 </script>
 
 <div class='w-full flex justify-center'>
-  <main class='main-leather flex flex-col space-y-4 max-w-2xl w-full'>
+  <main class='main-leather flex flex-col space-y-4 max-w-2xl w-full mt-4 mb-4'>
     <Heading tag='h1' class='h-leather mb-2'>New Article</Heading>
     {#if isEditing}
       <form>
