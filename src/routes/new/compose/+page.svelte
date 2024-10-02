fc4e57592716a0c8cf25b1a13b1727ff6880ce69
buttercat1791 <mjjurkoic@gmail.com>
2024-10-01 20:23:53 -0500

    Rename view page to compose

---
+++ src/routes/new/compose/+page.svelte
@@ -0,0 +1,14 @@
+<script lang='ts'>
+  import Preview from "$lib/components/Preview.svelte";
+  import { parser } from "$lib/parser";
+  import { Heading } from "flowbite-svelte";
+
+  
+</script>
+
+<div class='w-full flex justify-center'>
+  <main class='main-leather flex flex-col space-y-4 max-w-2xl w-full mt-4 mb-4'>
+    <Heading tag='h1' class='h-leather mb-2'>Compose</Heading>
+    <Preview rootIndexId={$parser.getRootIndexId()} allowEditing={true} />
+  </main>
+</div>
