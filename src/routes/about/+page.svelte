01d66a57eedd9cb02cf1fde8740b4a33d533b5d7
Silberengel <silberengel7@protonmail.com>
2024-10-19 07:18:32 -0700

    Adding link to GitHub page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,6 +10,6 @@
     <p>Alexandria is a <a href="https://wikistr.com/nkbip-01" class="text-indigo-600 underline">Nostr Knowledge Base (NKB)</a> and a reader for long-form articles.
       It is produced by <a href="https://wikistr.com/gitcitadel-project" class="text-indigo-600 underline">GitCitadel</a>.</p>
 
-    <p>Please submit support issues on the <a href="https://gitworkshop.dev/r/naddr1qvzqqqrhnypzplfq3m5v3u5r0q9f255fdeyz8nyac6lagssx8zy4wugxjs8ajf7pqy88wumn8ghj7mn0wvhxcmmv9uqq5emfw33kjarpv3jkcs83wav" class="text-indigo-600 underline">GitWorkshop page</a>.</p>
+    <p>Please submit support issues on the <a href="https://gitworkshop.dev/r/naddr1qvzqqqrhnypzplfq3m5v3u5r0q9f255fdeyz8nyac6lagssx8zy4wugxjs8ajf7pqy88wumn8ghj7mn0wvhxcmmv9uqq5emfw33kjarpv3jkcs83wav" class="text-indigo-600 underline">GitWorkshop page</a> and follow us on <a href="https://github.com/ShadowySupercode/gitcitadel" class="text-indigo-600 underline">GitHub</a>.</p>
   </main>
 </div>
